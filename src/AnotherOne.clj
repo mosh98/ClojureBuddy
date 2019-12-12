bfdbeef72ad01e47f660d12bbffc8ddcb7f6b5b0
moslehmahamud <mosleh.edu@gmail.com>
2019-12-12 17:05:59 +0100

    ANotherOne

--- src/AnotherOne.clj
+++ src/AnotherOne.clj
@@ -1,17 +1,27 @@
-(ns AnotherOne)
+(ns AnotherOne
+  (:import (java.io FileReader File)))
 
 ;(vector [variable value])
 
-(defmacro safe [list]
-
-
- (try
-   `(do ~list)
-  (catch Exception e (println (str "caught exception: " (.toString e))))
+(defmacro safe ( [expr]
+                (try
+                  `(do ~expr)
+                  (catch Exception e ( (str "caught exception: " (.getMessage e) )))
+                  )
+                )
+(
+ [[elem1 elem2]expr]
+  `(do elem1 elem2)
+ `(do ~expr)
  )
+
+
+
   )
 
 
 
-(def v (safe (/ 1 0)))
+(def v (safe (/ 2 0)))
 (println v )
+;(def v (safe [s (FileReader. (File. "file.txt"))] (.read s)))
+
